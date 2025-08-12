dc129171d5fbfff02b050b386353c37c49a5a28f
Mariana_k <m.koval000@gmail.com>
2025-08-12 14:43:53 +0300

    change layaut

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,16 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Roboto } from "next/font/google";
 import "./globals.css";
 import TanStackProvider from "@/components/TanStackProvider/TanStackProvider";
 
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const roboto = Roboto({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-roboto",
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -43,7 +40,7 @@ const RootLayout = ({
 }>) => {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${roboto.variable}`}>
         <TanStackProvider>
           <Header />
           {children}
--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -5,28 +5,28 @@ type Props = {
   params: Promise<{ slug: string[] }>;
 };
 
-// export const generateMetadata = async ({ params }: Props) => {
-//   const { slug } = await params;
-//   const tag = slug[0] === "All" ? undefined : slug[0];
+export const generateMetadata = async ({ params }: Props) => {
+  const { slug } = await params;
+  const tag = slug[0] === "All" ? undefined : slug[0];
 
-//   return {
-//     title: tag || "All",
-//     description: "notes",
-//     openGraph: {
-//       title: tag || "All",
-//       description: "notes",
-//       images: [
-//         {
-//           url: "/public/image.png",
-//           width: 1200,
-//           height: 630,
-//           alt: "logo",
-//         },
-//       ],
-//       url: `https://notehub/notes/filter/${tag}`,
-//     },
-//   };
-// };
+  return {
+    title: tag || "All",
+    description: "notes",
+    openGraph: {
+      title: tag || "All",
+      description: "notes",
+      images: [
+        {
+          url: "/public/image.png",
+          width: 1200,
+          height: 630,
+          alt: "logo",
+        },
+      ],
+      url: `https://notehub/notes/filter/${tag}`,
+    },
+  };
+};
 
 const NotesFilter = async ({ params }: Props) => {
   const { slug } = await params;
